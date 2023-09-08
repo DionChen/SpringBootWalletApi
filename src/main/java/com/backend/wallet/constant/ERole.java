dfb0e16f793d0aaf565edc5e615ebd272ad0fe6c
DionChen <s8800242002@gmail.com>
2023-09-08 17:01:45 +0800

    feature: add user po

---
+++ src/main/java/com/backend/wallet/constant/ERole.java
@@ -0,0 +1,7 @@
+package com.backend.wallet.constant;
+
+public enum ERole {
+    ROLE_USER,
+
+    Role_ADMIN
+}
---
+++ src/main/java/com/backend/wallet/models/po/Role.java
@@ -0,0 +1,19 @@
+package com.backend.wallet.models.po;
+
+import com.backend.wallet.constant.ERole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
