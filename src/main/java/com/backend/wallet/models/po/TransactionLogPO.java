da821142e7eba7fcdab743a51f4378b05bb10e4b
DionChen <s8800242002@gmail.com>
2023-09-11 20:20:23 +0800

    fix: transactionlog po

--- src/main/java/com/backend/wallet/models/po/TransactionLogPO.java
+++ src/main/java/com/backend/wallet/models/po/TransactionLogPO.java
@@ -3,14 +3,17 @@ package com.backend.wallet.models.po;
 import com.backend.wallet.constant.TransactionType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
-@Table
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Table(name = "transactionLog")
+@Entity
 public class TransactionLogPO {
 
     @Id
